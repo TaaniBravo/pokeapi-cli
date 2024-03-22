5dee9526d6e72b9891273cf8fb349fde6b0503c1
Taani Bravo <70364612+TaaniBravo@users.noreply.github.com>
2024-03-21 20:33:09 -0700

    feat: Working cli tool. Some Thoughts on different features: - Pretty printing the json would be nice without tools like jq and bat.

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+pub mod api;
+
+use api::pokemon::get_data;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "PokeAPI CLI", about = "Interact with PokeAPI via CLI")]
+struct Args {
+    /// Category can be pokemon or item.
+    #[structopt(short, long)]
+    category: String,
+
+    /// Key can be either the name or id of subject in category.
+    #[structopt(short, long)]
+    key: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::from_args();
+
+    let value = get_data(args.category, args.key).await;
+
+    match value {
+        Ok(value) => println!("{}", value),
+        Err(e) => println!("{}", e),
+    }
+}
